4f8bbd8acbef6464153210e5e7783dc1ba22962b
Matt Seccafien <mseccafien@gmail.com>
2020-12-15 10:01:50 +0100

    custom webpack

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
 
-/* eslint-disable import/no-extraneous-dependencies */
-const withTM = require('next-transpile-modules')(
-  // All of the packages will resolve to our monorepo so we can match that path.
-  [path.resolve(__dirname, '../../packages')],
-);
+const withTM = require('next-transpile-modules')([
+  path.resolve(__dirname, '../minou'),
+]);
 
-// Set target for compatibility with Vercel/Now deployments:
-// Source: https://github.com/vercel/vercel/blob/master/errors/now-next-no-serverless-pages-built.md
 module.exports = withTM({
   target: 'serverless',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      minou: path.resolve(__dirname, '../minou'),
+    };
+    return config;
+  },
 });
