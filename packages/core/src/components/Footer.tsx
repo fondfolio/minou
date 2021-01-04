625450e706f1d31e6af7f8e715ef4b400ca3ca6c
Matt Seccafien <mseccafien@gmail.com>
2021-01-04 20:08:20 +0100

    fix node version and footer

--- packages/core/src/components/Footer.tsx
+++ packages/core/src/components/Footer.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Icons} from '@minou/icons';
 
 import {Navigation} from './Navigation';
-import {Layout} from './Layout';
 import {Text} from './Text';
 import {Link} from './Link';
 import {ButtonGroup} from './ButtonGroup';
@@ -10,6 +9,8 @@ import {Bullet} from './Bullet';
 import {Flex, Box} from './Box';
 import {Label} from './Label';
 import {Icon} from './Icon';
+import {Container} from './Container';
+import {Section} from './Section';
 
 const FONDFOLIO_DOMAIN = 'https://fondfolio.com';
 
@@ -29,33 +30,35 @@ export function Footer() {
           {content: 'Blog', size: 'small', url: `${FONDFOLIO_DOMAIN}/blog`},
         ]}
       />
-      <Layout>
-        <Flex
-          alignItems={['flex-start', 'center']}
-          flexDirection={['column', 'row']}
-        >
-          <Label pb={0} small>
-            Copyright 2020 Fondfolio Inc.
-          </Label>
-          <Box display={['none', 'block']}>
-            <Bullet />
-          </Box>
-          <Text small italic pb={[2, 0]} pr={3}>
-            Proudly made in Toronto by Cartogram
-          </Text>
-          <ButtonGroup>
-            <Link unstyled url="https://twitter.com/fondfolio">
-              <Icon color="primary" icon={Icons.Twitter} />
-            </Link>
-            <Link unstyled url="https://www.instagram.com/fondfolio">
-              <Icon color="primary" icon={Icons.Instagram} />
-            </Link>
-            <Link unstyled url="https://www.facebook.com/fondfolio/">
-              <Icon color="primary" icon={Icons.Facebook} />
-            </Link>
-          </ButtonGroup>
-        </Flex>
-      </Layout>
+      <Container>
+        <Section>
+          <Flex
+            alignItems={['flex-start', 'center']}
+            flexDirection={['column', 'row']}
+          >
+            <Label pb={0} small>
+              Copyright 2020 Fondfolio Inc.
+            </Label>
+            <Box display={['none', 'block']}>
+              <Bullet />
+            </Box>
+            <Text small italic pb={[2, 0]} pr={3}>
+              Proudly made in Toronto by Cartogram
+            </Text>
+            <ButtonGroup>
+              <Link unstyled url="https://twitter.com/fondfolio">
+                <Icon color="primary" icon={Icons.Twitter} />
+              </Link>
+              <Link unstyled url="https://www.instagram.com/fondfolio">
+                <Icon color="primary" icon={Icons.Instagram} />
+              </Link>
+              <Link unstyled url="https://www.facebook.com/fondfolio/">
+                <Icon color="primary" icon={Icons.Facebook} />
+              </Link>
+            </ButtonGroup>
+          </Flex>
+        </Section>
+      </Container>
     </>
   );
 }
